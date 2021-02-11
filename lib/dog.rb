be76ab2d85660876fd43d9c2f9a819c20a080026
Daniel Sung <DanielChanSung@gmail.com>
2021-02-11 00:00:06 -0500

    commit tree f21ece46aecb836b12c25d12404664e578a2b035

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,42 @@
-# Add your code here
+    # Add your code here
+    require 'pry'
+    class Dog
+        
+        attr_accessor :name
+        @@all = []
+
+        def initialize(name)
+            @name = name
+           save
+            
+        end
+        
+        def self.all
+            @@all
+        end
+
+        def self.clear_all
+            @@all.clear
+        end
+
+        def self.print_all
+            @@all.collect { |namedog| puts namedog.name }
+       
+  
+     
+        end
+    
+        def save
+            @@all << self
+        
+        end
+
+    end
+
+
+    # Dog.new("Pluto")
+    # Dog.new("Fido")
+    # Dog.new("Apple")
+
+    # Dog.print_all
+        
